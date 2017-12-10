e275f3d4653bcc9b557bef771b984af116905048
mason82121 <mason82121@gmail.com>
2017-12-10 21:27:54 +0800

    1
    1

--- resources/js/main.js
+++ resources/js/main.js
@@ -52,42 +52,46 @@ map.addMarker({
 });   
     
 /*animate*/
-    $('.js--wp-1').waypoint(function(direction){
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, {
-        offset:'50%'
-    });
+    // $('.js--wp-1').waypoint(function(direction){
+    //     $('.js--wp-1').addClass('animated fadeIn');
+    // }, {
+    //     offset:'50%'
+    // });
     
-    $('.js--wp-2').waypoint(function(direction){
-        $('.js--wp-2').addClass('animated fadeInLeft');
-    }, {
-        offset:'50%'
-    });
+    // $('.js--wp-2').waypoint(function(direction){
+    //     $('.js--wp-2').addClass('animated fadeInLeft');
+    // }, {
+    //     offset:'50%'
+    // });
     
-    $('.js--wp-3').waypoint(function(direction){
-        $('.js--wp-3').addClass('animated fadeInRight');
-    }, {
-        offset:'50%'
-    });
+    // $('.js--wp-3').waypoint(function(direction){
+    //     $('.js--wp-3').addClass('animated fadeInRight');
+    // }, {
+    //     offset:'50%'
+    // });
     
-    $('.js--wp-4').waypoint(function(direction){
-        $('.js--wp-4').addClass('animated shake');
-    }, {
-        offset:'50%'
-    });
+    // $('.js--wp-4').waypoint(function(direction){
+    //     $('.js--wp-4').addClass('animated shake');
+    // }, {
+    //     offset:'50%'
+    // });
     
-    $('.js--wp-5').waypoint(function(direction){
-        $('.js--wp-5').addClass('animated zoomInDown');
-    }, {
-        offset:'50%'
-    });
+    // $('.js--wp-5').waypoint(function(direction){
+    //     $('.js--wp-5').addClass('animated zoomInDown');
+    // }, {
+    //     offset:'50%'
+    // });
     
-    $('.js--wp-6').waypoint(function(direction){
-        $('.js--wp-6').addClass('animated jello');
-    }, {
-        offset:'50%'
-    });
+    // $('.js--wp-6').waypoint(function(direction){
+    //     $('.js--wp-6').addClass('animated jello');
+    // }, {
+    //     offset:'50%'
+    // });
 
     
     
 });
+
+
+
+
